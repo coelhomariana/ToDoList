a1d47053c8a1d24677f9376fab927927dfd79336
Mariana Coelho <coelhomariana@outlook.com>
2020-10-10 19:20:45 -0300

    Adicionada a rota de tarefas atrasadas

--- src/controller/TaskController.js
+++ src/controller/TaskController.js
@@ -1,6 +1,8 @@
 const { json } = require("body-parser");
 const TaskModel = require("../model/TaskModel");
 
+const currentDate = new Date();
+
 class TaskController {
   async create(req, res) {
     const task = new TaskModel(req.body);
@@ -72,6 +74,20 @@ class TaskController {
       })
       .catch((error) => {
         return res.status(500).json(error);
+      });
+  }
+
+  async late(req, res) {
+    await TaskModel.find({
+      when: { $lt: currentDate },
+      macaddress: { $in: req.body.macaddress },
+    })
+      .sort("when")
+      .then((response) => {
+        return res.status(200).json(response);
+      })
+      .catch((error) => {
+        res.status(500).json(error);
       });
   }
 }
